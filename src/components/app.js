29c6f78fc644a368420717ff393ca6eb6faf838a
Veronika Dzhambazova <veronika.dz@sbtech.com>
2019-01-31 15:33:51 +0200

    finish project

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
-import SearchBar from './search-bar'
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-    state = {images: []};
+    state = { images: [] };
 
     onSearchSubmit = async (term) => {
         const response = await unsplash.get('https://api.unsplash.com/search/photos', {
-            params: {query: term},
-            headers: {
-                Authorization: 'Client-ID 6b79ac7746043208252776dfa70259f5a9072ce9251eee1e1ab37f79fc6cd45c'
-            }
+            params: { query: term }
         });
 
         this.setState({ images: response.data.results });
@@ -18,12 +16,12 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="ui container" style={{marginTop: '10px'}}> 
+            <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images}></ImageList>
             </div>
         );
     }
 }
 
-export default App
+export default App;
